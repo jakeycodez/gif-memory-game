0eca0be420412fbef46bc992f5f096d25283972d
Jacob Martella <jacobmartella@gmail.com>
2022-06-16 20:02:02 -0400

    Updated axios functionality

--- src/App.js
+++ src/App.js
@@ -1,80 +1,106 @@
-import './App.css'
-import { useState } from 'react'
-import SingleCard from './component/SingleCard/SingleCard'
-import { useEffect } from 'react'
+import "./App.css";
+import { useState } from "react";
+import SingleCard from "./component/SingleCard/SingleCard";
+import { useEffect } from "react";
+import axios from "axios";
 
 
 
-const cardImages = [
-  { "src": "/img/lebron-1.gif", matched: false},
-  { "src": "/img/lebron-2.gif", matched: false},
-  { "src": "/img/lebron-3.gif", matched: false},
-  { "src": "/img/lebron-4.gif", matched: false},
-  { "src": "/img/lebron-5.gif", matched: false},
-  { "src": "/img/lebron-6.gif", matched: false}
-]
 
 function App() {
-  const [cards, setCards] = useState([])
-  const [turns, setTurns] = useState(0)
-  const [choiceOne, setChoiceOne] = useState(null)
-  const [choiceTwo, setChoiceTwo] = useState(null)
-  const [disabled, setDisabled] = useState(false)
+  // const [theme, setTheme] = useState()
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
+  const [disabled, setDisabled] = useState(false);
+
+
+
+  useEffect(() => {
+    
+    const userInput = "pizza";
+    
+
+    axios
+      .get(`https://api.giphy.com/v1/gifs/search?api_key=lb0AoRDXWhAPC7TD9WjVdMMTC3h5CBat&q=${userInput}&limit=6`)
+      .then((result) => {
+        console.log(result)
+        result.data.data.forEach((gif) => {
+          cardImages.push({
+            src: gif.images.downsized_large.url,
+            matched: false
+          }
+            )
+        })
+      })
+      console.log(cardImages);
+   
+  
+  }, [])  
+
+  const cardImages = [
+    { src: "/img/lebron-1.gif", matched: false },
+    { src: "/img/lebron-2.gif", matched: false },
+    { src: "/img/lebron-3.gif", matched: false },
+    { src: "/img/lebron-4.gif", matched: false },
+    { src: "/img/lebron-5.gif", matched: false },
+    { src: "/img/lebron-6.gif", matched: false },
+  ];
 
   //shuffle Cards
   const shuffleCards = () => {
     const shuffledCards = [...cardImages, ...cardImages]
       .sort(() => Math.random() - 0.5)
-      .map((card) => ({ ...card, id: Math.random() }))
+      .map((card) => ({ ...card, id: Math.random() }));
 
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setCards(shuffledCards);
+    setTurns(0);
+  };
 
-      setChoiceOne(null)
-      setChoiceTwo(null)
-      setCards(shuffledCards)
-      setTurns(0)
-  }
+  // handle a choice by the user
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  };
 
-  // handle a choice by the user 
-    const handleChoice = (card) => {
-      choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
-    }
+  // Compare two selected cards
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      setDisabled(true);
 
-    // Compare two selected cards 
-    useEffect(() => {
-      if (choiceOne && choiceTwo) {
-        setDisabled(true)
-
-        if (choiceOne.src === choiceTwo.src) {
-          setCards(prevCards => {
-            return prevCards.map(card => {
-              if (card.src === choiceOne.src) {
-                return {...card, matched: true}
-              } else {
-                return card
-              }
-            })
-          })
-          resetTurn()
-        } else {
-          setTimeout(() =>  resetTurn(), 1500)
-        }
+      if (choiceOne.src === choiceTwo.src) {
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if (card.src === choiceOne.src) {
+              return { ...card, matched: true };
+            } else {
+              return card;
+            }
+          });
+        });
+        resetTurn();
+      } else {
+        setTimeout(() => resetTurn(), 1500);
       }
-    }, [choiceOne, choiceTwo])
-
-    console.log(cards)
-
-    // reset choices and increases turn
-    const resetTurn = () => {
-      setChoiceOne(null)
-      setChoiceTwo(null)
-      setTurns(prevTurns => prevTurns + 1)
-      setDisabled(false)
     }
+  }, [choiceOne, choiceTwo]);
 
-    // start game automatically on page load
-    useEffect(() => {
-      shuffleCards()
-    }, [])
+  // console.log(cards);
+
+  // reset choices and increases turn
+  const resetTurn = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns((prevTurns) => prevTurns + 1);
+    setDisabled(false);
+  };
+
+  // start game automatically on page load
+  useEffect(() => {
+    shuffleCards();
+  }, []);
 
   return (
     <div className="App">
@@ -82,13 +108,14 @@ function App() {
       <button onClick={shuffleCards}>New Game</button>
 
       <div className="card-grid">
-        {cards.map(card => (
-          <SingleCard 
-          key={card.id} 
-          card={card}
-          handleChoice={handleChoice}
-          flipped={card === choiceOne || card === choiceTwo || card.matched}
-          disabled={disabled}/>
+        {cards.map((card) => (
+          <SingleCard
+            key={card.id}
+            card={card}
+            handleChoice={handleChoice}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+            disabled={disabled}
+          />
         ))}
       </div>
       <p>Turns: {turns}</p>
@@ -96,4 +123,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
